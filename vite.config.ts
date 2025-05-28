ad359eceea872c8bec9c617163954e5910460cef
George <gcharris@calpoly.edu>
2025-05-27 18:20:21 -0700

    fixed repo base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/capstone-aerospace/",  // repo name
+  base: "/capstoneaero/",  // repo name
   plugins: [react()],
 });
