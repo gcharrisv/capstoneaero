8ba4979e4645cbc2fd52a836b47455c67bd0abc4
George <gcharris@calpoly.edu>
2025-05-27 18:15:58 -0700

    Switcg to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import Aircraft from './pages/Aircraft';
@@ -11,7 +11,7 @@ import './styles/tokens.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
@@ -20,6 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="careers" element={<Careers />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
