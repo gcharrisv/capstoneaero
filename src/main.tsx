1bd91b3ba80a4fc0f27bbb1eabfd873cc1694d78
George <gcharris@calpoly.edu>
2025-05-27 18:07:46 -0700

    Fix CSS imports

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import Home from './pages/Home';
 import Aircraft from './pages/Aircraft';
 import Team from './pages/Team';
 import Careers from './pages/Careers';
-import './styles.css';
+import './styles/index.css';
+import './styles/tokens.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
