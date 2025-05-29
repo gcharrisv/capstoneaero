c8b33e9aa436384c063b179349ceb9d5f8a2eead
George <gcharris@calpoly.edu>
2025-05-29 15:38:42 -0700

    fixed loading bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import './styles/index.css';
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
